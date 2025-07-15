0394085996952e0c28f696213fd0fea329cb6385
SyDyJo <gadyamb.re@gmail.com>
2025-07-15 16:47:26 +0200

    feat: implémentation du composant RecipeCard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
-import SearchBar from './components/SearchBar/SearchBar'
+import RecipeCard from './components/RecipeCard/RecipeCard'
+// import SearchBar from './components/SearchBar/SearchBar'
 
 function App() {
   return (
     <div className="w-full h-screen flex justify-center items-center bg-amber-50">
-      <SearchBar placeholder={'Rechercher une recette, un ingrédient, ...'} />
+      {/* <SearchBar placeholder={'Rechercher une recette, un ingrédient, ...'} /> */}
+      <RecipeCard />
     </div>
   )
 }
---
+++ frontend/src/components/RecipeCard/RecipeCard.tsx
@@ -0,0 +1,58 @@
+import pictureRecipe from '../../assets/img-card.png'
+
+export default function RecipeCard() {
+  return (
+    <div className="bg-white rounded-2xl w-96">
+      <div className="w-full h-full relative">
+        <img
+          className="w-full h-1/3 object-cover rounded-t-2xl static"
+          src={pictureRecipe}
+          alt="visuel de la recette"
+        />
+        <div className="absolute right-0 top-0 bg-emerald-500 rounded-full px-3 py-1 m-4">
+          <p className="text-white">10 min</p>
+        </div>
+      </div>
+      <div className="p-5">
+        <h3 className="text-3xl font-bold py-4">Titre de la recette</h3>
+        <h4 className="text-lg text-gray-400 text-bold py-4">RECETTE</h4>
+        <p>
+          Mettre les glaçons à votre goût dans le blender, ajouter le lait, la
+          crème de coco, le jus de 2 citrons et le sucre. Mixer jusqu'à avoir la
+          consistence désirée.
+        </p>
+        <div>
+          <h4 className="text-lg text-gray-400 text-bold py-4">IGRÉDIENTS</h4>
+          <div>
+            <ul className="grid grid-cols-2 gap-4">
+              <li>
+                <p>Lait de coco</p>
+                <p className="text-gray-400">400ml</p>
+              </li>
+              <li>
+                <p>Jus de citron</p>
+                <p className="text-gray-400">2</p>
+              </li>
+              <li>
+                <p>Crème de coco</p>
+                <p className="text-gray-400">4 cuillères</p>
+              </li>
+              <li>
+                <p>Sucre</p>
+                <p className="text-gray-400">20g</p>
+              </li>
+              <li>
+                <p>Glaçons</p>
+                <p className="text-gray-400">2</p>
+              </li>
+              <li>
+                <p>Glaçons</p>
+                <p className="text-gray-400">2</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
